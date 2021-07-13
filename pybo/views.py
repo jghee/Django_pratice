aeb5876fe79e171f10d3bb71658f0edb23cb0c69
jghee <ghj171937@gmail.com>
2021-07-13 09:58:56 +0900

    임시 수정

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm
