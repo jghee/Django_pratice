33887f32bcafc07b254870e1afdc149e6e3e636c
jghee <ghj171937@gmail.com>
2021-07-14 17:05:24 +0900

    mysql 설정

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'NAME': 'pybo',
+        'USER': 'pybo',
+        'PASSWORD': '1111',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
+
     }
 }
 
